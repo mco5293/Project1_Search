83357ddf1e59a5a880f867aea2458a9d9f776ca8
Kylie Clark <kylieclark2005@gmail.com>
2026-09-25 15:56:03 -0400

    adds ucs search and formats answers. needs debugged

--- solution_q2.py
+++ solution_q2.py
@@ -10,7 +10,7 @@ def read_input():
         start_state = input_state.split(",") #make start state into an array seperated by the commas
         for i in range(4): #make the # of missionaries and cannibals an int
             start_state[i] = int(start_state[i])
-        return start_state
+    return start_state
 
 #checks to make sure the state follows all of the rules
 #not negative or greater than 3
@@ -30,11 +30,12 @@ def is_valid(state):
 
     #checks for more cannibals than missionaries if m is not 0
     if(c_left>m_left and m_left != 0): #LEFT
-        return False;
+        return False
     elif(c_right>m_right and m_right!= 0): #RIGHT
-        return False;     
-    return True; #if it makes it this far, it survived the checks
+        return False    
+    return True #if it makes it this far, it survived the checks
 
+#creates the Node class for path traceback
 class Node:
     def __init__(self, state, parent, cost):
         self.state = state
@@ -52,6 +53,7 @@ def next_node(queue):
     if(len(queue) == 0):
         print("Queue empty.")
         return None
+    #finds the first node in the queue with the lowest cost
     else:
         lowest = 0
         for i in range(len(queue)):
@@ -61,22 +63,123 @@ def next_node(queue):
         del queue[lowest]
         return node
 
-#expand the node, adding expansions to the queue
-def expand_node(queue, node):
-    #checks if state is valid before attempting to expand
-    if(is_valid(state)):
-        #extract each piece of the state array
-        for i in state:
-            m_left = state[0]
-            c_left = state[1]
-            m_right = state[2]
-            c_right = state[3]
-            boat = state[4]
+def get_valid_next_states(node):
+    #extract each piece of the state array
+    state = node.state
+    for i in state:
+        m_left = state[0]
+        c_left = state[1]
+        m_right = state[2]
+        c_right = state[3]
+        boat = state[4]
 
-if __name__=='__main__':
+    potential_states=[]
+    moves=[(1,1), (1,0), (0,1), (2,0), (0,2)] #all valid moves from one side to the other (M,C)
+
+    for move_m, move_c in moves:
+        if boat=='L':
+            new_state=[m_left - move_m, c_left - move_c, m_right + move_m, c_right + move_c, 'R']
+        elif boat=='R':
+            new_state=[m_left + move_m, c_left + move_c, m_right - move_m, c_right - move_c, 'L']
+
+        if is_valid(new_state):
+            potential_states.append(new_state)
+    
+    return potential_states
+
+def get_costA(state, new_state):
+    #extract each piece of the state arrays
+    for i in state:
+        m_left = state[0]
+        c_left = state[1]
+        m_right = state[2]
+        c_right = state[3]
+        boat = state[4]
+    
+    for i in new_state:
+        m_left_new = new_state[0]
+        c_left_new = new_state[1]
+        m_right_new = new_state[2]
+        c_right_new = new_state[3]
+        boat_new = new_state[4]
+
+    #Each missionary costs 2, each cannibal costs 1
+    if boat == 'L':
+        move_m = m_left - m_left_new
+        move_c = c_left - c_left_new
+    elif boat == 'R':
+        move_m = m_right - m_right_new
+        move_c = c_right - c_right_new
+    return 2 * move_m + move_c
+
+def get_costB(state):
+    #extract each piece of the state array
+    for i in state:
+        m_left = state[0]
+        c_left = state[1]
+        m_right = state[2]
+        c_right = state[3]
+        boat = state[4]
+
+    #Right to left boat trip costs 1, left to right boat trip costs 2
+    if boat == 'L':
+        return 2
+    elif boat == 'R':
+        return 1
+
+#expand the node, adding expansions to the queue
+def expand_node(queue, node, cost_type):
+    #extract each piece of the state array
+    state = node.state
+    for i in state:
+        m_left = state[0]
+        c_left = state[1]
+        m_right = state[2]
+        c_right = state[3]
+        boat = state[4]
+    
+    #Creates nodes for all new potential state and adds them to the priority queue
+    potential_states = get_valid_next_states(node)
+    if cost_type == "A":
+        for new_state in potential_states:
+            new_cost = get_costA(state, new_state)
+            new_node = Node(state = new_state, parent = node, cost = node.cost + new_cost)
+            add_queue(queue, new_node)
+    elif cost_type == "B":
+        for new_state in potential_states:
+            new_cost = get_costB(state)
+            new_node = Node(state = new_state, parent = node, cost = node.cost + new_cost)
+            add_queue(queue, new_node)
+
+#traces the path back from the goal state to the start state, then reverses order for printing purposes
+def get_path(node):
+    path = []
+    while node is not None:
+        path.append(node.state)
+        node = node.parent
+    path.reverse()
+    return path
+
+#core function, loops through queue until goal state is reached
+def ucs_cost(queue, start_node, cost_type):
+    add_queue(queue, start_node)
+
+    while queue:
+        node = next_node(queue)
+
+        if node.state == [0, 0, 3, 3, 'R']:
+            path = get_path(node)
+            print(f"The solution of Q2.1 (UCS, cost model {cost_type}) is:\nSolution Path: {path}\nTotal cost = {node.cost}\nNumber of node expansions = {len(visited)}")
+            return
+
+        expand_node(queue, node, cost_type)
+
+    print(f"No solution found with cost type {cost_type}.")
+
+if __name__ == '__main__':
     queue = []
-    start_state = read_input
+    start_state = read_input()
     start_node = Node(state = start_state, parent = None, cost = 0)
 
-    ucs_costA(queue, start_node)
-    ucs_costB(queue, start_node)
+    ucs_cost(queue, start_node, "A")
+    ucs_cost(queue, start_node, "B")
